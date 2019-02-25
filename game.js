dd22c264c7bd58d059a66fb752cd8887305eff06
Weslley Braga <wesbragagt@gmail.com>
2019-02-25 11:49:36 -0600

    added a check function for the submit button and when the countdown reaches 0

--- game.js
+++ game.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
-setInterval(decrement, 1000);
+
+intervalId = setInterval(decrement, 1000);
+
 })
+
+// GLOBAL TIME COUNTDOWN
 var countdown = 30;
 var intervalId;
 
 function decrement(){
     countdown--;
     $("#time").html(countdown + "s");
+
+    if(countdown == 0){
+        stop();
+        check();
+    }
     
 }
 function stop(){
     clearInterval(intervalId);
+}
+
+function check(){
+    stop();
+    alert("results");
+
 }
