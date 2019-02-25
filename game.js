bb79daab64cf99ff9783ac41618fb78ad8420277
Weslley Braga <wesbragagt@gmail.com>
2019-02-25 11:45:15 -0600

    time countdown is ready

--- game.js
+++ game.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    
+setInterval(decrement, 1000);
 })
+var countdown = 30;
+var intervalId;
+
+function decrement(){
+    countdown--;
+    $("#time").html(countdown + "s");
+    
+}
+function stop(){
+    clearInterval(intervalId);
+}
