12b836aee3de30b60612e433d61fbee25010296f
Weslley Braga <wesbragagt@gmail.com>
2019-02-26 14:58:15 -0600

    added checker to check for the correct input radio selects

--- game.js
+++ game.js
@@ -1,128 +1,102 @@
-$(document).ready(function(){
-populate();
-// intervalId = setInterval(decrement, 1000);
+$(document).ready(function() {
+  populate();
+  // intervalId = setInterval(decrement, 1000);
+});
 
-})
+// function check submit button
+function check() {
+  stop();
+  var correct = 0;
+  var incorrect = 0;
+  var selected = [];
+  $("input:checked").each(function() {
+    selected.push($(this).val());
+  });
+
+  for (var i = 0; i < trivia.length; i++) {
+    if (selected[i] === trivia[i].correct) {
+      correct++;
+    } else {
+      incorrect++;
+    }
+  }
+
+  console.log("correct: " + correct, "incorrect: " + incorrect);
+}
 
 // TRIVIA, create an array with an object and all the questions that are going to be used
 
-
 var trivia = [
-    {
-        question: "What's Goku's real name?",
-        options: ["Kakarot", "Gohan", "Bardock", "Trunks"],
-        correct: "Kakarot"
-    },
+  {
+    question: "What's Goku's real name?",
+    options: ["Kakarot", "Gohan", "Bardock", "Trunks"],
+    correct: "Kakarot"
+  },
 
-    {
-        question: "Which Android did Vegeta kill?",
-        options: ["Android 17", "Android 18", "Android 20", "Android 16"],
-        correct: "Android 20"
-    },
+  {
+    question: "Which Android did Vegeta kill?",
+    options: ["Android 17", "Android 18", "Android 20", "Android 16"],
+    correct: "Android 20"
+  },
 
-    {
-        question: "Who Killed Frieza?",
-        options: ["Gohan", "Raditz", "Vegeta", "Future Trunks"]
-    }
-
-    
+  {
+    question: "Who Killed Frieza?",
+    options: ["Gohan", "Raditz", "Vegeta", "Future Trunks"],
+    correct: "Future Trunks"
+  }
 ];
+// populate the screen with questions and their options to pick
+function populate() {
+  for (var i = 0; i < trivia.length; i++) {
+    var convertI = (i + 1).toString();
+    var questionId = "question" + convertI;
 
-function populate(){
-    for(var i = 0; i < trivia.length;i++){
-        var convertI = (i+1).toString();
-        var questionId = "question" + convertI;
-        console.log(questionId);
-        
-        // QUESTION
-        var h2 = $("<h2/>").attr({id: questionId, class: "question"});
-        h2.text(convertI+". " + trivia[i].question);
-        $("#quiz").append(h2);
-        
-        var newDivId = "div" + convertI;
+    // QUESTION
+    var h2 = $("<h2/>").attr({ id: questionId, class: "question" });
+    h2.text(convertI + ". " + trivia[i].question);
+    $("#quiz").append(h2);
 
-        var newDiv = $("<div class = 'options'/>").attr("id", newDivId);
-        $("#" + questionId).after(newDiv);
-        
-        
-        // INPUTS
-        for(var j = 0; j < trivia[i].options.length; j++){
-            var convertJ = (j+1);
-            var inputId = "input" + convertJ;
-            var labelId = "label" + convertJ;
-            
-            
-            var label = $("<label class = 'label'>").attr({for: questionId, name: questionId, id: labelId}).text(trivia[i].options[j]);
-            
-            label.text(trivia[i].options[j]);
-            $("#"+ newDivId).append(label);
+    // DIV WHICH I WILL HAVE MY OPTIONS NESTED
+    var newDivId = "div" + convertI;
 
-            var input = $("<input class= 'input' type = 'radio'/>").attr({name: questionId, value: trivia[i].options[j]});
-            $("#" + newDivId).append(input);
-            
-            // console.log(label);
-            // console.log(input);
-            
-            
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    var newDiv = $("<div class = 'options'/>").attr("id", newDivId);
+    $("#" + questionId).after(newDiv);
+
+    // LABELS AND INPUTS
+    for (var j = 0; j < trivia[i].options.length; j++) {
+      var convertJ = j + 1;
+      var inputId = "input" + convertJ;
+      var labelId = "label" + convertJ;
+
+      var label = $("<label class = 'label'>")
+        .attr({ for: questionId, name: labelId, id: labelId })
+        .text(trivia[i].options[j]);
+
+      label.text(trivia[i].options[j]);
+      $("#" + newDivId).append(label);
+
+      var input = $("<input class= 'input' type = 'radio'/>").attr({
+        name: questionId,
+        value: trivia[i].options[j]
+      });
+      $("#" + newDivId).append(input);
     }
-    // trivia.questions.forEach(function(index, value){
-    //     var questionNumber = "question" + (value+1);
-        
-    //     for(var i = 0; i < 4; i++){
-    //         // var convertI = (i+1).toString();
-    //         // var questionNumber = "question" + convertI;
-    //         var textInput = trivia.answerChoice.question1[i];
-    //         console.log(textInput);
-            
-    //         var input = $("<input type='radio'/>").addClass("options").attr({name: questionNumber});
-    //         input.text(textInput);
-    //         $("#" + questionNumber).append(input);
-    //         console.log(input);
-            
-            
-    //     }
-    // })
-
-    
-    
-    
-    
+  }
 }
-
-
-
 
 // GLOBAL TIME COUNTDOWN
 var countdown = 30;
 var intervalId;
 
-function decrement(){
-    countdown--;
-    $("#time").html(countdown + "s");
+function decrement() {
+  countdown--;
+  $("#time").html(countdown + "s");
 
-    if(countdown == 0){
-        stop();
-        check();
-    }
-    
-}
-function stop(){
-    clearInterval(intervalId);
-}
-
-function check(){
+  if (countdown == 0) {
     stop();
-    alert("results");
-
+    check();
+  }
+}
+function stop() {
+  clearInterval(intervalId);
 }
