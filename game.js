1ac421b1ba17af94e87af91d35c7f17fdc947d63
Weslley Braga <wesbragagt@gmail.com>
2019-02-26 13:25:45 -0600

    managed to add labels and inputs to each question

--- game.js
+++ game.js
@@ -4,35 +4,75 @@ populate();
 
 })
 
-// TRIVIA, create an array with all the questions that are going to be used
+// TRIVIA, create an array with an object and all the questions that are going to be used
 
-// var trivia = {
-//     questions:[
-//         "What's Goku's real name?", 
-//         "Who is the first DBZ fighter to turn Super Sayian 2?"
-//     ],
-//     answer: ["Kakarot", "Gohan"]
-
-// }
 
 var trivia = [
     {
         question: "What's Goku's real name?",
         options: ["Kakarot", "Gohan", "Bardock", "Trunks"],
         correct: "Kakarot"
+    },
+
+    {
+        question: "Which Android did Vegeta kill?",
+        options: ["Android 17", "Android 18", "Android 20", "Android 16"],
+        correct: "Android 20"
+    },
+
+    {
+        question: "Who Killed Frieza?",
+        options: ["Gohan", "Raditz", "Vegeta", "Future Trunks"]
     }
 
     
 ];
 
 function populate(){
-    for(var i = 0; i < trivia.questions.length;i++){
+    for(var i = 0; i < trivia.length;i++){
         var convertI = (i+1).toString();
-        var questionNumber = "question" + convertI;
+        var questionId = "question" + convertI;
+        console.log(questionId);
         
-        var h2 = $("<h2/>").addClass("question").attr("id", questionNumber);
-        h2.text(convertI+". " + trivia.questions[i]);
-        $("#quiz").append(h2);  
+        // QUESTION
+        var h2 = $("<h2/>").attr({id: questionId, class: "question"});
+        h2.text(convertI+". " + trivia[i].question);
+        $("#quiz").append(h2);
+        
+        var newDivId = "div" + convertI;
+
+        var newDiv = $("<div class = 'options'/>").attr("id", newDivId);
+        $("#" + questionId).after(newDiv);
+        
+        
+        // INPUTS
+        for(var j = 0; j < trivia[i].options.length; j++){
+            var convertJ = (j+1);
+            var inputId = "input" + convertJ;
+            var labelId = "label" + convertJ;
+            
+            
+            var label = $("<label class = 'label'>").attr({for: questionId, name: questionId, id: labelId}).text(trivia[i].options[j]);
+            
+            label.text(trivia[i].options[j]);
+            $("#"+ newDivId).append(label);
+
+            var input = $("<input class= 'input' type = 'radio'/>").attr({name: questionId, value: trivia[i].options[j]});
+            $("#" + newDivId).append(input);
+            
+            // console.log(label);
+            // console.log(input);
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
         
         
     }
